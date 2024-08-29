78c4248d7d3fcf86dfa9bc4dc945f2596414ae2d
Jack Wolfe <jwolfe@jmbullion.com>
2024-08-29 16:11:45 -0500

    init

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import "./globals.css";
+import Providers from "@/app/providers";
+import Header from "@/components/header";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className="">
+        <div className="container mx-auto px-4 max-w-6xl">
+          <Providers>
+            <Header />
+            {children}
+          </Providers>
+        </div>
+      </body>
+    </html>
+  );
+}
---
+++ src/app/topics/[slug]/posts/new/page.tsx
@@ -0,0 +1,5 @@
+"use client";
+
+export default function PostCreatePage() {
+  return <div>PostCreatePage</div>;
+}
